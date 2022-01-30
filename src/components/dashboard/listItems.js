4672f0b9151d254eb0ecb0d410373c4610de3970
Jonas Samaitis <jonas.samaitis@imsmaxims.com>
2022-01-30 01:51:04 +0000

    loginaddititon

--- src/components/dashboard/listItems.js
+++ src/components/dashboard/listItems.js
@@ -12,6 +12,7 @@ import AssignmentIcon from '@mui/icons-material/Assignment';
 
 export const mainListItems = (
   <div>
+    
     <ListItem button>
       <ListItemIcon>
         <DashboardIcon />
